b4fc57d6b0075f67340d6e9d212f0b12fac380d5
Cameron <camjfernandez@gmail.com>
2022-12-14 11:30:28 -0800

    cookie clicker basic

--- midterm/midterm/lib/main.dart
+++ midterm/midterm/lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+
+var stats =  CookieStats();
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual, overlays: []);
   runApp(MyApp());
+}
+
+class CookieStats {
+  num _cookies = 0;
+  num get cookieCount {
+    return _cookies;
+  }
+  set cookieCount (num cookie) {
+    setState(
+      () => _cookies += cookie
+      );
+  }
+
 }
 
 class MyApp extends StatelessWidget {
@@ -18,30 +36,66 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainMenu extends StatelessWidget {
+class MainMenu extends StatefulWidget {
   @override 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        toolbarHeight: 30,
+        toolbarHeight: 50,
         flexibleSpace: Center(
           child: Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            children: [
-              const Text("logo"),
-              const Text("apptitle"),
-              const Text("placeholder"),
+            children: const [
+              Text("logo"),
+              Text("apptitle"),
+              Text("placeholder"),
             ],
           ),
         ),
       ),
-      body: const Center(
-        child: Text("ahhh test"),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              
+              height: 500,
+              decoration: const BoxDecoration(
+                color: Colors.blueAccent,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text("$stats.cookieCount Cookies"),
+                  Container(
+                    height: 300,
+                    width: 300,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.brown,
+                    ),
+                  ),
+                ]
+              ),
+            ),
+            Column(children:[Text("placeholder"),],),
+          ],
+        ),
       ),
     );
   } 
- 
+ State<MainMenu> createState() => increment();
+}
+
+class increment extends State<MainMenu>{
+  setState(() => stats.cookieCount++);
 }
 
 
-class Game extends State
+class TestThing extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Text("placeholder");
+
+  }
+}
