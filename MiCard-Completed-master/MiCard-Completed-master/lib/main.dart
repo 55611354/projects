a0d67967c4154e585d3e15282a7004b9852d54c6
Cameron <camjfernandez@gmail.com>
2023-01-06 10:29:19 -0800

    setup gitignore, and Micard

---
+++ MiCard-Completed-master/MiCard-Completed-master/lib/main.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.orange,
+        body: SafeArea(
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage('images/picture.png'),
+            ),
+            Text(
+              'Cameron Fernandez',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 40.0,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                fontFamily: 'Source Sans Pro',
+                color: Colors.orange.shade100,
+                fontSize: 20.0,
+                letterSpacing: 2.5,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.orange.shade100,
+              ),
+            ),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.orange,
+                  ),
+                  title: Text(
+                    '+1 175 256 573',
+                    style: TextStyle(
+                      color: Colors.orange.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20.0,
+                    ),
+                  ),
+                )),
+            Card(
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.orange,
+                  ),
+                  title: Text(
+                    'realEmail@gmail.com',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.orange.shade900,
+                        fontFamily: 'Source Sans Pro'),
+                  ),
+                ))
+          ],
+        )),
+      ),
+    );
+  }
+}
